3edd1808e0526bf20c1868ced3a3a18565b0e613
Grace Lee <gracelee9901@gmail.com>
2022-12-29 01:34:41 -0800

    Fix typo and update HairSalonContext class

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,7 +4,8 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    // public DbSet<>  { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
   }
